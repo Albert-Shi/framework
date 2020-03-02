1579b366288eedc04530f8b49d598e9744a302ea
shishuheng <1136313573@qq.com>
2020-03-02 23:22:16 +0800

    自定义登录

--- src/main/java/com/shishuheng/webdemo/config/SecurityConfig.java
+++ src/main/java/com/shishuheng/webdemo/config/SecurityConfig.java
@@ -1,14 +1,20 @@
 package com.shishuheng.webdemo.config;
 
+import com.shishuheng.webdemo.security.MyAuthenticationFilter;
+import com.shishuheng.webdemo.security.MyAuthenticationProvider;
 import com.shishuheng.webdemo.service.UserService;
 import com.shishuheng.webdemo.service.base.MyAccessDeniedHandler;
 import com.shishuheng.webdemo.service.base.MyPasswordEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * @author shishuheng
@@ -25,16 +31,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private MyAccessDeniedHandler accessDeniedHandler;
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.formLogin();
         http.authorizeRequests().anyRequest().authenticated().and().exceptionHandling().accessDeniedHandler(accessDeniedHandler);
+//        MyAuthenticationFilter myAuthenticationFilter = new MyAuthenticationFilter();
+//        myAuthenticationFilter.setAuthenticationManager(authenticationManagerBean());
+//        MyAuthenticationProvider myAuthenticationProvider = new MyAuthenticationProvider();
+//        http.authenticationProvider(myAuthenticationProvider).addFilterAfter(myAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
---
+++ src/main/java/com/shishuheng/webdemo/security/MyAuthenticationToken.java
@@ -0,0 +1,31 @@
+package com.shishuheng.webdemo.security;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+/**
+ * @author shishuheng
+ */
+public class MyAuthenticationToken extends AbstractAuthenticationToken {
+    private Object principal;
+    private Object credentials;
+
+    public MyAuthenticationToken(Collection<? extends GrantedAuthority> authorities, Object principal, Object credentials) {
+        super(authorities);
+        setAuthenticated(true);
+        this.principal = principal;
+        this.credentials = credentials;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return credentials;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return principal;
+    }
+}
