84da7ea1aef7b3aede212293cf361f17a151f3c7
shishuheng <1136313573@qq.com>
2020-03-10 14:30:49 +0800

    配置中心加入

---
+++ service-configuration/src/test/java/com/shishuheng/framework/service/configuration/ServiceZookeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shishuheng.framework.service.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceZookeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
